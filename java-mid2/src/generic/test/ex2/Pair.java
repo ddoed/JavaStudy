93fbe7a0204700ddcdf1861e7811b308d71720ca
yimnabin <yimnabin20051022@gmail.com>
2025-01-13 13:32:01 +0900

    Generic1

---
+++ java-mid2/src/generic/test/ex2/Pair.java
@@ -0,0 +1,30 @@
+package generic.test.ex2;
+
+public class Pair<T,S> {
+    private T first;
+    private S second;
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
